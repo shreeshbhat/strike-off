5748ae742ef4f5c1ed282a256a9b8ca2097a8647
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2019-02-09 18:34:43 -0500

    initial commit

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,21 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+  shadow: true
+})
+export class AppHome {
+
+  render() {
+    return (
+      <div class='app-home'>
+        <stencil-route-link url='/profile/stencil'>
+          <button>
+            Profile page
+          </button>
+        </stencil-route-link>
+      </div>
+    );
+  }
+}
