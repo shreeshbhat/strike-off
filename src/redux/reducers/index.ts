a0a78ac867582eaabacf6f227cdae7fe2bdce164
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2019-03-03 21:28:01 -0800

    Remove redux

--- src/redux/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-
-import { combineReducers } from 'redux';
-
-const rootReducer = (combineReducers as any)({
-  app
-});
-
-export default rootReducer;
--- src/redux/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers/index';
-
-const configureStore = (preloadedState: any) =>
-  createStore(rootReducer, preloadedState);
-
-export { configureStore };
