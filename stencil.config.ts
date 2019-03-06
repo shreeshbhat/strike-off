5004f5ec63a3ad9511836e8eb43a0e1fadaa3327
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2019-03-06 07:46:22 -0800

    Add sass plugin and cache svg

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,15 +1,22 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  plugins: [
+    sass()
+  ],
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   outputTargets: [
     {
       type: 'www',
-      // uncomment the following line to disable service workers in production
-      // serviceWorker: null
+      serviceWorker: {
+        globPatterns: [
+          '**/*.{js,css,json,html,ico,png,svg}'
+        ]
+      }
     }
   ]
 };
