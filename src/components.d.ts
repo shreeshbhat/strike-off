b411a6da14859823105301738558cc4ba6a8f5ef
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2019-03-15 22:24:47 -0400

    Update stencil version

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,10 +7,10 @@
 
 import '@stencil/core';
 
-import '@stencil/router';
-import '@stencil/state-tunnel';
 import '@ionic/core';
 import 'ionicons';
+import '@stencil/router';
+import '@stencil/state-tunnel';
 import {
   MatchResults,
 } from '@stencil/router';
