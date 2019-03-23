a5550ed9e3ba2393f238e3e5299cfcd4db64437e
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2019-03-22 23:36:41 -0400

    Add idb-keyval for storage

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,25 +1,9 @@
+import { get, set } from 'idb-keyval';
 
-
-import { default as PouchDB } from 'pouchdb-browser';
-//import PouchDB from 'pouchdb-browser';
-
-var db = new PouchDB('todos');
-//var remoteCouch = false;
-
-
-export function addTodo(todo: any) {
-  db.put(todo, function callback(err, result) {
-    if (!err) {
-      console.log('Successfully posted a todo!');
-      console.debug('Result : ' + result);
-    }
-  });
+export async function setTodos(todos: any) {
+  set('todos',todos);
 }
 
-export function showTodos() {
-  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-    if (!err) {
-      return(doc.rows);
-    }
-  });
+export async function getTodos() {
+  return get('todos');
 }
