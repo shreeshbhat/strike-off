c2439654437cb220614486a59b6db5dbb1422545
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2020-05-10 14:26:36 -0400

    Add labels to header buttons
    - Update stencil

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -87,11 +87,13 @@ export class AppRoot {
         <header>
           <so-button class="menu-button" fill="clear" onButtonClick={() => this.openMenu()}>
             <ion-icon name="menu" class="menu-icon"></ion-icon>
+            <span class="button-label">Menu</span>
           </so-button>
           <h1 class="app-header">Strike off</h1>
           {this.showUpdate ? (
             <so-button class="update-button" fill="clear" onButtonClick={() => this.update()}>
               <ion-icon name="download" class="menu-icon"></ion-icon>
+              <span class="button-label">Update</span>
             </so-button>
           ) : (
             <div></div>
